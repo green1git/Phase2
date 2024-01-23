759c5836d307e74dc21a6036a2c3b5d62f95f7da
MilesWeedon <98825066+MilesWeedon@users.noreply.github.com>
2024-01-23 22:52:15 +0000

    PCA working
    Next step is to remove points close to others, and plug the remaining points into the trajectory model, before measuring error

--- 15 Setting Data CSV files/PCA.m
+++ 15 Setting Data CSV files/PCA.m
@@ -1,8 +1,14 @@
+% Read output.csv 
 M = readmatrix("output.csv");
+
+% Label the 15 test conditions
 label = linspace(1, 15, 15);
 
-[V, Y] = pca(M); %Basis, coords
+% Perform PCA
+[V, Y] = pca(M); % Basis, coords
 
+% Plot the data against the two principle components with the highest
+% varience
 figure;
 scatter(Y(:, 1), Y(:, 2), 45, label, "filled");
 
--- 15 Setting Data CSV files/extraction.py
+++ 15 Setting Data CSV files/extraction.py
@@ -5,41 +5,39 @@ import numpy as np
 # Set the folder path to the current working directory
 folder_path = '.'
 
-# Initialize an empty list to store DataFrames
-dataframes = np.zeros([15, 4])
+# Initialize an empty array to store test condition averages
+avgs = np.zeros([15, 4])
 
+# Initialise count - used for the averaging later
 count = 0
 
 
 # Iterate through all files in the folder
 for filename in os.listdir(folder_path):
-    if filename.endswith('.csv'):
-        file_path = os.path.join(folder_path, filename)
-
-        # Read the CSV file
-        df = pd.read_csv(file_path, header=None)
-
-        # Extract the last four columns
-        df_last_four = np.array(df.iloc[:, -4:])
-
-        
-        # Append the DataFrame to the list
-        dataframes = np.add(dataframes, df_last_four)
-        
-        count+=1
-
-
-# Concatenate all DataFrames along rows
-#concatenated_df = pd.concat(dataframes)
-
-# Compute the row-wise average
-#row_wise_average = concatenated_df.mean(axis=1)
+    if filename.endswith('.csv'): #Filter by the csvs
+        if filename != "output.csv": #Ignore any pre-existing outputs
+            file_path = os.path.join(folder_path, filename)
+    
+            # Read the CSV file
+            df = pd.read_csv(file_path, header=None) #No header 
+    
+            # Extract the last four columns
+            df_last_four = np.array(df.iloc[:, -4:])
+    
+            
+            # Append the current reading to the array
+            avgs = np.add(avgs, df_last_four)
+            
+            # Iterate count
+            count+=1
 
 
-row_wise_average = dataframes / count
+# Take the average, using the count variable
+avgs = avgs / count
 
-
+# Print resultr
 print("Row-wise average of the final four columns from each CSV file:")
-print(row_wise_average)
+print(avgs)
 
-np.savetxt("output.csv", row_wise_average, delimiter=",")
+# Save output as output.csv
+np.savetxt("output.csv", avgs, delimiter=",")
