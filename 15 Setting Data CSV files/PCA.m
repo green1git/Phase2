e2eb16c2ee4dc4807e4e95d4825456f8646ebd1a
MilesWeedon <98825066+MilesWeedon@users.noreply.github.com>
2024-01-24 16:38:33 +0000

    Beginning to implement DBSCAN
    Also implemented cooks distance, however the problem is that we've fitted a linear model to the data in PCA space, which appears non-linear

--- 15 Setting Data CSV files/PCA.m
+++ 15 Setting Data CSV files/PCA.m
@@ -4,13 +4,29 @@ M = readmatrix("output.csv");
 % Label the 15 test conditions
 label = linspace(1, 15, 15);
 
-% Perform PCA
+%% Perform PCA
 [V, Y] = pca(M); % Basis, coords
 
 % Plot the data against the two principle components with the highest
 % varience
 figure;
+Y = Y(:, 1:2); % Keep two principle components with the highest varience
 scatter(Y(:, 1), Y(:, 2), 45, label, "filled");
-
 colorbar;
-colormap hot;
+colormap turbo;
+
+%% Perform DBSCAN
+clusterDBSCAN.estimateEpsilon(Y,2,15);
+clusterDBSCAN.discoverClusters(Y,2,2);
+
+clusterer = clusterDBSCAN('MinNumPoints',2,'Epsilon',0.45447, ...
+    'EnableDisambiguation',false);
+[idx,cidx] = clusterer(Y);
+plot(clusterer,Y,idx)
+
+idx = dbscan(Y,0.403,1);
+figure;
+gscatter(Y(:,1),Y(:,2), idx, [], [], 45);
+mdl = fitlm(Y(:, 1),Y(:, 2));
+
+plotDiagnostics(mdl,'cookd')
