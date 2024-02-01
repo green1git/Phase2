e12d4bbebc0d1b8454419a0a92cb40d1deeead57
harrywilliams1002 <98824015+harrywilliams1002@users.noreply.github.com>
2024-02-01 15:33:06 +0000

    Create IterativeMethod.m

---
+++ IterativeMethod'/IterativeMethod.m
@@ -0,0 +1,28 @@
+data = readtable("ball11pp_for_sfs.csv")
+
+num_conditions = 10; 
+combinations = nchoosek(1:15, num_conditions);
+
+optimized_results = cell(size(combinations, 1), 3); % First column for constants, second for fval, third for index of combinations
+
+for i = 1:size(combinations, 1)
+    % sselect subset of data based on the current combination
+    current_combination = combinations(i, :);
+    subset_data = data(current_combination, :);
+
+    % relevant columns for this subset
+    R = subset_data.ReynoldsNumber;
+    alpha = subset_data.SpinRatio;
+    C_l_observed = subset_data.CoefficientOfLift;
+    C_d_observed = subset_data.CoefficientOfDrag;
+
+    % optimization with the subset of data
+    [optimized_constants, fval] = fmincon(@(constants) residuals_function(constants, R, alpha, C_l_observed, C_d_observed), initial_guess, [], [], [], [], lb, ub, [], options);
+
+    % Store the results
+    optimized_results{i, 1} = optimized_constants;
+    %optimized_results{i, 2} = fval;
+    optimized_results{i, 2} = current_combination
+end
+
+% optimized_results contains the optimized constants and objective function values for each combination
