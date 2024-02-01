f364a65ab3e82a611b76d0a8a90ac35e718a2294
MilesWeedon <98825066+MilesWeedon@users.noreply.github.com>
2024-02-01 20:53:52 +0000

    Working PCA2

--- 15 Setting Data CSV files/PCA2.m
+++ 15 Setting Data CSV files/PCA2.m
@@ -1,8 +1,9 @@
 % Read output.csv 
-M = readmatrix("output.csv");
+M = readmatrix("../27 Setting Data/output.csv"); % 27 setting data
+%M= readmatrix("output.csv"); % 15 setting data
 
 % Take transpose, so that each of the 15 test conditions are the features
-
+M = M';
 
 %% Perform PCA
 [V, Y, eigenvalues, tsquared, explained] = pca(M); % Basis, coords
@@ -22,3 +23,9 @@ end
 
 %% Normalize the feature importance
 feature_importance = feature_importance / sum(feature_importance);
+
+%% Append labels and sort
+
+feature_importance(:, 2) = 1:size(feature_importance, 1);
+removal_order = sortrows(feature_importance);
+disp(removal_order(:, 2));
---
+++ 27 Setting Data/extraction.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import numpy as np
+
+# Set the folder path to the current working directory
+folder_path = '.'
+
+# Initialize an empty array to store test condition averages
+avgs = np.zeros([27, 4])
+
+# Initialise count - used for the averaging later
+count = 0
+
+
+# Iterate through all files in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith('.csv'): #Filter by the csvs
+        if filename != "output.csv": #Ignore any pre-existing outputs
+            file_path = os.path.join(folder_path, filename)
+    
+            # Read the CSV file
+            print(file_path)
+            df = pd.read_csv(file_path, header=None) #No header 
+    
+            # Extract the last four columns
+            df_last_four = np.array(df.iloc[:, -4:])
+    
+            
+            # Append the current reading to the array
+            avgs = np.add(avgs, df_last_four)
+            
+            # Iterate count
+            count+=1
+
+
+# Take the average, using the count variable
+avgs = avgs / count
+
+# Print resultr
+print("Row-wise average of the final four columns from each CSV file:")
+print(avgs)
+
+# Save output as output.csv
+np.savetxt("output.csv", avgs, delimiter=",")
