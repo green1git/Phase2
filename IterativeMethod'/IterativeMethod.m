504524f521a49f78c53cbd9d63edc0105320594d
harrywilliams1002 <98824015+harrywilliams1002@users.noreply.github.com>
2024-02-02 13:03:45 +0000

    Update IterativeMethod.m

--- IterativeMethod'/IterativeMethod.m
+++ IterativeMethod'/IterativeMethod.m
@@ -5,6 +5,16 @@ combinations = nchoosek(1:15, num_conditions);
 
 optimized_results = cell(size(combinations, 1), 3); % First column for constants, second for fval, third for index of combinations
 
+% Set the initial guess for the constants (a1,a2,a3,b1,b2,b3,...,d2) Random
+initial_guess = ones(1, 11);
+
+% Set the options for fmincon sqp algo fine for this
+options = optimoptions('fmincon', 'Display', 'iter', 'Algorithm', 'sqp');
+
+%Infinite bounds decision varibales can take
+lb = -Inf * ones(1, 11); % Lower bounds
+ub = Inf * ones(1, 11);  % Upper bounds
+
 for i = 1:size(combinations, 1)
     % sselect subset of data based on the current combination
     current_combination = combinations(i, :);
